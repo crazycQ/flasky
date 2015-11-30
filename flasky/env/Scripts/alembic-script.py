8686619a3a5f04d8b8e4454918487896ed9870d6
ecq <fortunegets@live.com>
2015-11-30 23:50:43 +0800

    1st

---
+++ flasky/env/Scripts/alembic-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\fortu\Documents\Visual Studio 2015\Projects\test_flask\test_flask\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'alembic==0.8.3','console_scripts','alembic'
+__requires__ = 'alembic==0.8.3'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('alembic==0.8.3', 'console_scripts', 'alembic')()
+    )
---
+++ flasky/env/Scripts/mako-render-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\fortu\Documents\Visual Studio 2015\Projects\test_flask\test_flask\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Mako==1.0.3','console_scripts','mako-render'
+__requires__ = 'Mako==1.0.3'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('Mako==1.0.3', 'console_scripts', 'mako-render')()
+    )
